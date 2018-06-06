ed1dba2fc73d1641ff91809f4f84a156de45abc2
Vincent Caruso <carusov@ohsu.edu>
2018-06-06 23:29:47 +0000

    Add more parameter options

--- scripts/make_projects_batch.sh
+++ scripts/make_projects_batch.sh
@@ -3,6 +3,9 @@
 # Create several lyve-SET projects by calling 'make_snp_project.sh' and reading
 # the input variables from a file
 
+REFDIR=~/references
+OUTDIR=$PWD
+
 # Parse command-line options
 while [[ $# -gt 0 ]]
 do
@@ -12,8 +15,21 @@ do
 	-f|--file_name)
 	    FNAME="$2"
 	    shift;;
+	-w|--work_dir)
+	    WDIR="$2"
+	    shift;;
+	-r|--ref_dir)
+	    REFDIR="$2"
+	    shift;;
+	-o|--out_dir)
+	    OUTDIR="$2"
+	    shift;;
 	-h|--help|*)
 	    printf "\nUSAGE: make_projects_batch.sh -f --file_name variable_file_name\n"
+	    printf "Options: \t[default]"
+	    printf "\n-w --work_dir \t[current directory] \t\tworking sample directory"
+	    printf "\n-r --ref_dir \t[%s] \treference assembly directory" "$REFDIR"
+	    printf "\n-o --out_dir \t[current directory] \t\toutput directory\n\n"
 	    exit;;
 	*)
 
@@ -28,6 +44,16 @@ if [ -z $FNAME ]; then
     exit
 fi
 
+WDIR=$(readlink -f "$WDIR")
+REFDIR=$(readlink -f "$REFDIR")
+OUTDIR=$(readlink -f "$OUTDIR")
+
+# Create output directory if necessary
+if [ ! -d "$OUTDIR" ];
+then
+    mkdir "$OUTDIR"
+fi
+
 while read -r s r; do
-    make_snp_project.sh -s $s -r $r
+    make_snp_project.sh -s $s -w "$WDIR" -r $r -d "$REFDIR" -t "$OUTDIR"/$s
 done < "$FNAME"
