0638d1a4d6adc8eb43303653f7be85ba487f059f
Vincent Caruso <carusov@ohsu.edu>
2018-06-19 01:04:36 +0000

    Fix logic to avoid repetition, add project guessing

--- scripts/download_sample_basespace.sh
+++ scripts/download_sample_basespace.sh
@@ -8,9 +8,12 @@
 ### the BaseMount API.
 
 # Define default variables values
-BASESPACE=~/osphl/basespace
-MODE=regex
+BASESPACE=~/basespace
+TARGET=$PWD
+#MODE=regex
 
+
+### SETUP ###
 # Parse command-line options
 while [[ $# -gt 0 ]]
 do
@@ -29,28 +32,27 @@ do
 	-t|--target_dir)
 	    TARGET="$2"
 	    shift;;
-	-m|--mode)
-	    MODE="$2"
-	    shift;;
 	-r|--run)
 	    RUN="$2"
 	    shift;;
-	-h|--help|*)
-	    printf "\nUSAGE: copy_from_basespace.sh -n sample_name_pattern\n"
+	-h|--help)
+	    printf "\nUSAGE: download_sample_basespace.sh -n sample_name_pattern\n"
 	    printf "\nOptions: \t[default]"
-	    printf "\n-p --project \t[Validation] \t\tproject name"
-	    printf "\n-t --target \t[current directory] \ttarget directory"
-	    printf "\n-b --basespace \t[~/osphl/basespace/] \tbasespace mount point"
-	    printf "\n-m --mode \t[regex] \tmatching mode"
-	    printf "\n-r --run \t[] \trun name (only used for exact matching)"
+	    printf "\n-p --project \t[guess from sample name pattern] \tproject name"
+	    printf "\n-t --target \t[current directory] \t\t\ttarget directory"
+	    printf "\n-b --basespace \t[~/basespace] \t\t\t\tbasespace mount point"
+	    printf "\n-r --run \t[NONE] \t\t\trun name (only used for exact matching)"
 	    printf "\n\n"
 	    exit;;
 	*)
+	    printf "\nERROR: Invalid script usage. Here is the proper usage for this script:\n"
+	    download_sample_basespace.sh -h
+	    exit 1;;
 
-	;;
     esac
     shift
 done
+
 
 
 BASESPACE=$(readlink -f "$BASESPACE")
@@ -59,82 +61,212 @@ BASESPACE=$(readlink -f "$BASESPACE")
 # Check parameters
 if [ -z "$PATTERN" ]
 then
+    
     printf "\nERROR: You must specify a sample name pattern.\n\n"
     exit 1;
-elif [ -z "$PROJECT" ]
-then
-    printf "\nERROR: You must specify a project name.\n\n"
-    exit 1;
-elif [ "$MODE" == "exact" ] && [ -z "$RUN" ]
-then
-    printf "\nERROR: You must specify a run name when using exact matching mode.\n\n"
-    exit 1;
-elif [ "$MODE" == "regex" ] && [ ! -z "$RUN" ]
-then
-    printf "\nWARNING: The run name is ignored when using regex matching mode.\n\n"
-elif [ "$MODE" != "exact" ] && [ "$MODE" != "regex" ]
-then
-    printf "\nERROR: The mode must be either 'regex' or 'exact'.\n\n"
-    exit 1;
+
 fi
+    
+if [ -z "$PROJECT" ]
+then
+    
+    # Try to guess the project from the sample name pattern
+    if [[ "$PATTERN" =~ "PNUSAC" ]]
+    then
+	
+	PROJECT="PRJNA239251"
+	
+    elif [[ "$PATTERN" =~ "PNUSAE" ]]
+    then
+	
+	PROJECT="PRJNA218110"
+	
+    elif [[ "$PATTERN" =~ "PNUSAL" ]]
+    then
+	
+	PROJECT="PRJNA212117"
+	
+    elif [[ "$PATTERN" =~ "PNUSAS" ]]
+    then
+	
+	PROJECT="PRJNA230403"
+	
+    elif [[ "$PATTERN" =~ "PNUSAF" ]]
+    then
+	
+	PROJECT="PRJNA266293"
+	
+    else
+	
+	printf "\nERROR: Could not guess the project from the sample name pattern.\n"
+	printf "Please specify a project name and try again.\n\n"
+	exit 1;
+	
+    fi
+fi
+
+if [ -z "$RUN" ]
+then
+    
+    MODE=regex
+
+else
+
+    MODE=exact
+
+fi
+
+
+### MAIN SCRIPT ###
 
 # Check to see if basespace is mounted
 if [ ! "$(ls -A $BASESPACE)" ]
 then
+    
     basemount -c routine "$BASESPACE"
+    
 fi
 
 # Make sure the project exists
 if [ ! -d "$BASESPACE"/Projects/"$PROJECT" ];
 then
-    printf "\nERROR: Project '${PROJECT}' does not exist. Double-check the name and try again.\n\n"
+    
+    printf "\nERROR: Project ${PROJECT} does not exist. Double-check the project name and try again.\n\n"
     exit 1;
+    
 else
+    
     SDIR="$BASESPACE"/Projects/"$PROJECT"/Samples/
-    SAMPLES=$(ls "$SDIR" | grep "$PATTERN")
+
 fi
 
 
+# Replace '_' with '-' to match MiSeq file names
+pattern=${PATTERN//_/-}
+samples=$(ls "$SDIR" | grep "$PATTERN")
+
+# Check to see if any matching samples were found
+if [ -z "$samples" ]
+then
+    
+    printf "No matching samples were found. Nothing was downloaded.\n\n"
+    exit 2
+    
+fi
+
 # Create the target directory if necessary
-if [ ! -d "$TARGET" ]; then
+if [ ! -d "$TARGET" ]
+then
+    
     mkdir -p "$TARGET"
+    
 fi
 
 TARGET=$(readlink -f "$TARGET")
 
+# Clean up any partially downloaded files from a previous interrupted session
+rm -f "$TARGET"/temp_R*.gz
 
-echo "$SAMPLES" | while read -r s
-do
-    # Replace '_' with '-' in sample names to match MiSeq file naming
-    s_file=${s//_/-}
 
-    # Check to see if files already exist before copying/downloading
-    if [ -f "$TARGET"/"$s_file"*R1*.gz ] && [ -f "$TARGET"/"$s_file"*R2*.gz ]
-    then
-	printf "Sample '%s' already exists locally\n" "$s"
+if [ "$MODE" = "regex" ]
+then
 
-    else
-	next=$(ls "$SDIR"/"$s"/Files/ | grep _R1)
-
-	if [[ ( $MODE == "regex" ) || ( $MODE == "exact"  &&  $next =~ "$RUN" ) ]]
+    # Iterate over all samples matching PATTERN in reverse order (newest first)
+    echo "$samples" | sort -r | while read -r s
+    do
+	
+	name=$(ls "$SDIR"/"$s"/Files | grep "_R1")
+	name=${name%%_*}
+	
+	# Check to see if sample already exists locally
+	if [ -f "$TARGET"/"$name"*R1*.fastq.gz ] &&
+	       [ -f "$TARGET"/"$name"*R2*.fastq.gz ]
 	then
-	    if [ ! -f "$TARGET"/"$s_file"*R1*.gz ]
+	    
+	    printf "Sample %s already exists locally\n" "$s"
+	    
+	else
+	    # Download any missing FASTQ files
+	    if [ ! -f "$TARGET"/"$name"*R1*.gz ]
 	    then
-		newname=${next//_S*001\./_R1\.}
+		
+		newname=${name}_R1.fastq.gz
 		cp "$SDIR"/"$s"/Files/*R1*.gz "$TARGET"/temp_R1.gz
 		mv "$TARGET"/temp_R1.gz "$TARGET"/"$newname"
+		chmod 664 "$TARGET"/"$newname"
+		
 	    fi
 	    
-	    if [ ! -f "$TARGET"/"$s_file"*R2*.gz ]
+	    if [ ! -f "$TARGET"/"$name"*R2*.gz ]
 	    then
-		newname=${next//_S*001\./_R2\.}
+		
+		newname=${name}_R2.fastq.gz
 		cp "$SDIR"/"$s"/Files/*R2*.gz "$TARGET"/temp_R2.gz
 		mv "$TARGET"/temp_R2.gz "$TARGET"/"$newname"
-
-		s_file=($s_file)
-		chmod 664 "$TARGET"/${s_file[0]}*.gz
-		printf "Finished downloading sample %s\n" "$s"
+		chmod 664 "$TARGET"/"$newname"
+		
 	    fi
+
+	    printf "Finished downloading sample %s\n" "$s"
+	    
+	fi
+    done
+
+elif [ "$MODE" = "exact" ]
+then
+
+    found=0
+
+    # Check to see if sample already exists locally
+    pattern=${PATTERN//_/-}
+    if [ $(ls "$TARGET" | grep "$pattern".*"$RUN" | wc -l) -eq 2 ]
+    then
+	
+	printf "Sample %s from run %s already exists locally\n" "$PATTERN" "$RUN"
+
+    else
+	# Iterate over samples in reverse order (newest to oldest)
+	while read -r s
+	do
+	    
+	    name=$(ls "$SDIR"/"$s"/Files | grep "_R1")
+	    name=${name%%_*}
+	    
+	    if [[ $name =~ "$RUN" ]]
+	    then
+		found=1
+		
+		if [ ! -f "$TARGET"/"$name"*R1*.gz ]
+		then
+		    
+		    newname=${name}_R1.fastq.gz
+		    cp "$SDIR"/"$s"/Files/*R1*.gz "$TARGET"/temp_R1.gz
+		    mv "$TARGET"/temp_R1.gz "$TARGET"/"$newname"
+		    chmod 664 "$TARGET"/"$newname"
+		    
+		fi
+		
+		if [ ! -f "$TARGET"/"$name"*R2*.gz ]
+		then
+		    
+		    newname=${name}_R2.fastq.gz
+		    cp "$SDIR"/"$s"/Files/*R2*.gz "$TARGET"/temp_R2.gz
+		    mv "$TARGET"/temp_R2.gz "$TARGET"/"$newname"
+		    chmod 664 "$TARGET"/"$newname"
+		    
+		fi
+
+		printf "Finished downloading sample %s\n" "$s"
+		break
+
+	    fi
+	done <<< $(echo "$samples" | sort -r)
+
+	if [ $found -ne 1 ]
+	then
+	    printf "No samples matching %s were found in run %s.\n" "$PATTERN" "$RUN"
+	    printf "Nothing was downloaded.\n"
 	fi
     fi
-done
+
+fi
